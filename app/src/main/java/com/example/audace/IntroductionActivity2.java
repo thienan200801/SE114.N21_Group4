87f745bab4604d6f04b0816378bffbaa076087a5
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2023-05-18 09:32:31 +0700

    feat: add intent for introduction pages

---
+++ app/src/main/java/com/example/audace/IntroductionActivity2.java
@@ -0,0 +1,26 @@
+package com.example.audace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IntroductionActivity2 extends AppCompatActivity {
+    private Button bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction2);
+        bt2=findViewById(R.id.bt2);
+        bt2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent t=new Intent(IntroductionActivity2.this,IntroductionActivity3.class);
+                startActivity(t);
+            }
+        });
+    }
+}
